f6eb44903261ef81f4167875ce9e1b66c9965314
jahanzaibshahid <jahanzaibshahid07@gmail.com>
2020-08-01 01:20:47 +0500

    first commit

---
+++ client-side/client-side/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace client_side
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new TcpClient("127.0.0.1", 8888);
+            NetworkStream ns = client.GetStream();
+            StreamReader sr = new StreamReader(ns);
+
+            textBox1servermessage.Text = "server >> " + sr.ReadLine(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2clientmessage.Text != "")
+            {
+                NetworkStream ns = client.GetStream();
+                StreamWriter sw = new StreamWriter(ns);
+                sw.WriteLine(textBox2clientmessage.Text);
+
+                sw.Flush();
+                sw.Close();
+                ns.Close();
+            }
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(8888);
+            server.Start();
+            Console.WriteLine("server started and waiting for client");
+            Socket socketforclients = server.AcceptSocket();
+
+            if(socketforclients.Connected)
+            {
+                // send message to client
+                NetworkStream ns = new NetworkStream(socketforclients);
+                StreamWriter sw = new StreamWriter(ns);
+                Console.WriteLine("Server >> Welcome Client ");
+                sw.WriteLine("Welcome Client");
+
+                sw.Flush();
+                // get message from client
+                StreamReader sr = new StreamReader(ns);
+                Console.WriteLine("Client >> " + sr.ReadLine());
+                sw.Close();
+                ns.Close();
+            }
+
+            socketforclients.Close();
+            Console.ReadLine();
+        }
+    }
+}
